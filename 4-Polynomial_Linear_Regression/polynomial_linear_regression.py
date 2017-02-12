69641c617fba4d93e1bac96cf384abde9af3ebad
Mehul Patel <mehul.21aug@gmail.com>
2017-02-11 21:06:20 -0500

    SVR

--- 4-Polynomial_Linear_Regression/polynomial_linear_regression.py
+++ 4-Polynomial_Linear_Regression/polynomial_linear_regression.py
@@ -1,10 +1,12 @@
 # Polynomial Regression
 
 # importing the libraries
-
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
+
 
 # importing the dataset
 
@@ -12,6 +14,40 @@ dataset = pd.read_csv('/Users/Leo/PycharmProjects/ML-Udemy/4-Polynomial_Linear_R
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-# Splitting the data into the training and test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Fitting Linear regression to dataset
+linearRegressor = LinearRegression()
+linearRegressor.fit(X, y)
+
+
+# Fitting Polynomial Linear Regression to dataset
+polynomialRegressor = PolynomialFeatures(degree=4)
+X_poly = polynomialRegressor.fit_transform(X)
+# instead of using the 1st degree features, we used X_poly.
+linearRegressor2 = LinearRegression()
+linearRegressor2.fit(X_poly, y)
+
+# Visualizing the linear regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, linearRegressor.predict(X), color='blue')
+plt.title('Truth or Bluff (Linear Regression Results)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualizing the polynomial regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, linearRegressor2.predict(polynomialRegressor.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff (Polynomial Linear Regression Results)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Predicting a new result with Linear Regression
+linearRegressor.predict(6.5)
+
+# Predicting a new result with Polynomial Linear Regression
+linearRegressor2.predict(polynomialRegressor.fit_transform(6.5))
---
+++ 5-Support_Vector_Regression/SVR.py
@@ -0,0 +1,39 @@
+# SVR
+
+# importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
+
+
+# importing the dataset
+
+dataset = pd.read_csv('/Users/Leo/PycharmProjects/ML-Udemy/5-Support_Vector_Regression/Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Feature scaling (SVR class doesn't provide that)
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# Fitting SVR to dataset
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y)
+
+# Predicting a new result
+# used inverse transform to make sure of feature scaling is inversed for prediction
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
+# Visualizing the SVR results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
